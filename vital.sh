63513e738f41884c29d384abfbcc09ea7d6b7578
mr-wh1tehat <clonescrt@mail.ru>
2023-08-12 15:44:33 +0500

    Create vital.sh

---
+++ vital.sh
@@ -0,0 +1,2 @@
+pacman -S grub efibootmgr networkmanager network-manager-applet wpa_supplicant dialog mtools dosfstools f86-input-synaptics tlp thinkfan reflector bluez bluez-utils blueberry cups xdg-utils xdg-user-dirs rsync inetutils dnsutils nfs-utils gvfs gvfs-smb openssh
+
